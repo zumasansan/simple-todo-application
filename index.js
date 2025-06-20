c2c7d4faafe57787835b632f4fc385490f0dec82
chipstar0424 <totsu0424@gmail.com>
2025-06-20 16:12:23 +0900

    テキスト6番の修正

--- index.js
+++ index.js
@@ -43,8 +43,9 @@ function onUpdateIsDone(task) {
     console.log("チェックボックスがクリックされました。", task);
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
-    const currentValue = task.isDone;
-    task.isDone = currentValue;
+    task.isDone = !task.isDone;
+    /*const currentValue = task.isDone;
+    task.isDone = currentValue;*/
 
     return task;
 }
